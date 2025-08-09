dfe90bbe3b8518b74c8ba451c1c5402ad46e373e
diniamo <diniamo53@gmail.com>
2025-08-09 06:25:55 +0200

    fix: warn instead of error on diff fail

--- internal/external/diff.go
+++ internal/external/diff.go
@@ -11,13 +11,13 @@ func Diff(from, to string)  {
 	command := exec.Command("dix", from, to)
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
-		
+
 	err := command.Run()
 	if err != nil {
 		if _, ok := err.(*exec.ExitError); ok {
-			log.Error("dix: non-zero exit code")
+			log.Warn("dix: non-zero exit code")
 		} else {
-			log.Errorf("Failed to run dix: %s", err)
+			log.Warnf("Failed to run dix: %s", err)
 		}
 	}
 }
